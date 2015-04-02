d647db9622a3849a0570299f962d61ba77e2924b
dryajov <dryajov@gmail.com>
2015-04-01 23:02:41 -0600

    added travis-ci integration

--- gulp/tasks/krma.js
+++ gulp/tasks/krma.js
@@ -4,7 +4,7 @@ var karma = require('karma').server;
 /**
  * Run test once and exit
  */
-gulp.task('test', function (done) {
+gulp.task('test', ['build'], function (done) {
     karma.start({
         configFile: __dirname + '/../../test/karma.conf.js',
         singleRun: true
